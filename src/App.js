60e6e1ee53c89cbfbdbfbfa847930c56c34e4d7e
brenda0323manon <brendamanon0526@gmail.com>
2024-01-05 14:08:05 -0500

    easy challenge complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-  import React, { Component } from 'react'
+  // import React, { Component } from 'react'
   
-  export default class App extends Component {
-    render() {
-      return (
-        <div className="App">
-        <h1>Name: Brenda Mañon</h1>
-        <p>Number: 1234567890</p>
-        <p>Date of Birth: May 126, 2001</p>
-    </div>
-      )
+  // export default class App extends Component {
+  //   render() {
+  //     return (
+  //       <div className="App">
+  //       <h1>Name: Brenda Mañon</h1>
+  //       <p>Number: 1234567890</p>
+  //       <p>Date of Birth: May 126, 2001</p>
+  //   </div>
+  //     )
+  //   }
+  // }
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      person: {}
     }
+
   }
-  
- 
-
-
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
